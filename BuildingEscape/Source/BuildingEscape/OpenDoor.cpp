ae26c7b1e895e3346756e0b2d9cc59a98d7eda00
TagnisDragon-PC\Tagni'sDragon <trantienhung77@yahoo.com>
2017-09-21 21:32:54 -0500

    - Added code for the room with 2 doors, which open by a volume trigger  - Added code for Line Tracing, represented by a red line in front of default pawn

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,59 @@
+// Copyright Hung Tran 2017.
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+#include "Engine/World.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	// Find the owning object
+	Owner = GetOwner();
+	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
+}
+
+void UOpenDoor::OpenDoor()
+{
+	Owner->SetActorRotation(FRotator(0.0f, OpenAngle, 0.0f));
+}
+
+void UOpenDoor::CloseDoor()
+{
+	Owner->SetActorRotation(FRotator(0.0f, 0.0f, 0.0f));
+}
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// Poll the Trigger Volume
+	if(PressurePlate->IsOverlappingActor(ActorThatOpens))
+	{
+		// If the ActorThatOpens is in the volume
+		OpenDoor();
+		LastDoorOpenTime = GetWorld()->GetTimeSeconds();
+	}
+
+	// Check if it's time to close the door
+	// If the time right now is passed the time the last door open by more than the delay
+	if (LastDoorOpenTime + DoorCloseDelay <= GetWorld()->GetTimeSeconds())
+	{
+		// Close the door
+		CloseDoor();
+	}
+}
+
